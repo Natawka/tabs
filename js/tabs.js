678bd2328ac7e9b009573aef7121422800416f3b
Natalya Kudrya <natawa.kudrya@mail.ru>
2016-04-02 16:36:15 +0300

    vbn

--- js/tabs.js
+++ js/tabs.js
@@ -1,9 +1,10 @@
 
 
-//alert('ji');
+
 $(function () {
+		
     var tabContainers = $('div.tabs > div'); 
-    tabContainers.hide().filter(':first').show(); // пр€чем все, кроме первого
+    tabContainers.hide().filter(':first').show(); // прячем все, кроме первого
    
     $('div.tabs ul.navigation a').click(function () {
         tabContainers.hide(); 
@@ -12,4 +13,38 @@ $(function () {
         $(this).addClass('selected'); 
         return false;
     }).filter(':first').click();
+	
+		var $element = $('.crib');   
+	    var $field = $element.find('input');
+  console.log($field);	
+  
+  
+		//CRIB
+	  
+		  $(':text').hover(function(){
+           				 
+		  $(this).next().show().animate({
+			 opacity: '0.7',
+			 marginLeft: "0.3in",
+			 paddingRight: "0.1in",
+			 paddingLeft: "0.1in"
+		  },1000);
+		 
+		 }, function(){
+			  $(this).next().hide();
+		  });
+	
+		var $click = $(':submit');
+	
+		$click.on('click', function(){
+			
+		$(':text').next().show().animate({
+			 opacity: '1',
+			 marginLeft: "0.2in",
+			 paddingRight: "-0.1in",
+			 paddingLeft: "-0.1in"
+		  },1000);;
+		
+		});	
+  
 });
